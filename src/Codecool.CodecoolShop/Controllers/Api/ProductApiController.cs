c641e8e3a46e78313d0af6a0cc5fbfe2d52297ab
Dfulop98 <dobo.fulop@icloud.com>
2022-10-17 19:16:44 +0200

    Add: route for product (sql ready)

---
+++ src/Codecool.CodecoolShop/Controllers/Api/ProductApiController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Codecool.CodecoolShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Codecool.CodecoolShop.Controllers.Api
+{
+    [Route("Products")]
+    [ApiController]
+    public class ProductApiController : Controller  
+    {
+        public List<Product> Products()
+        {
+            return JsonConvert.DeserializeObject<List<Product>>(Product);
+            ;
+        }
+    }
+}
