85b304a54d106c7b742010ca42955ae250f65fbb
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-10-17 11:33:25 +0200

    Add: js folder structure, start of IndexController

---
+++ src/Codecool.CodecoolShop/wwwroot/js/Controller/IndexController.js
@@ -0,0 +1,20 @@
+﻿// Add buttons for filtering
+function AddFilters() {
+    // dynamically generate categories
+    let categories = [];
+    let suppliers = [];
+
+    let productCards = document.querySelectorAll('.cardData');
+
+    productCards.forEach((card) => {
+        categories.push(card.dataset.category);
+        suppliers.push(card.dataset.supplier);
+    });
+}
+
+//Read in the activated filters and hide cards that don't match
+function FilterCards() {
+
+}
+
+AddFilters();
