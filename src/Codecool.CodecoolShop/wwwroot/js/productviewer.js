68762b42db0d5bf1e7da442cc2b29b6aa9d1ac84
robert <rnagy89@gmail.com>
2022-10-17 13:52:46 +0200

    Feat: Add click event to cards

---
+++ src/Codecool.CodecoolShop/wwwroot/js/productviewer.js
@@ -0,0 +1,10 @@
+﻿var selected = document.querySelectorAll(".card");
+for (var i = 0; i < selected.length; i++) {
+    selected[i].addEventListener('click', Examine);
+}
+
+
+function Examine() {
+    console.log(this);
+    return '/product/viewer/';
+}
