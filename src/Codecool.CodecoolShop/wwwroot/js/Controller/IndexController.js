e44258eb90ede37c12b319ce236cc7af87e09131
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-10-17 15:02:52 +0200

    Add: dom elements of the filters, functionality is still todo

--- src/Codecool.CodecoolShop/wwwroot/js/Controller/IndexController.js
+++ src/Codecool.CodecoolShop/wwwroot/js/Controller/IndexController.js
@@ -4,7 +4,29 @@ function AddFilters() {
     let categories = ["software", "hardware"];
     let suppliers = ["lenovo", "amazon"];
 
+    let mainContainer = document.querySelector('main');
+
+    let filterContainer = document.createElement('div');
+
+    mainContainer.appendChild(filterContainer);
+    filterContainer.id = "filterContainer";
+
     // Draw buttons
+    let categoryHeader = document.createElement('h3');
+    categoryHeader.innerText = "Categories";
+    filterContainer.appendChild(categoryHeader);
+    for (let category of categories) {
+        filterContainer.appendChild(ButtonFactory("category", category));
+    }
+
+    let supplierHeader = document.createElement('h3');
+    supplierHeader.innerText = "Suppliers";
+    filterContainer.appendChild(supplierHeader);
+    for (let supplier of suppliers) {
+        filterContainer.appendChild(ButtonFactory("supplier", supplier));
+    }
+
+    GatherFilters();
     // Event listeners for the buttons
     // parameterized query string to get the data from model file
     // fetch query for the categories and the suppliers
