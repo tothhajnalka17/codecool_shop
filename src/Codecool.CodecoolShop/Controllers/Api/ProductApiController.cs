c45b338376a20e777e4a2d7b9d8df35f93c27ab7
Dfulop98 <dobo.fulop@icloud.com>
2022-10-18 10:51:30 +0200

    Refactor: remove dao and service files and add apicontroller

--- src/Codecool.CodecoolShop/Controllers/Api/ProductApiController.cs
+++ src/Codecool.CodecoolShop/Controllers/Api/ProductApiController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Threading.Tasks;
 using Codecool.CodecoolShop.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -11,10 +13,18 @@ namespace Codecool.CodecoolShop.Controllers.Api
     [ApiController]
     public class ProductApiController : Controller  
     {
-        public List<Product> Products()
+        
+        public async Task<List<Product>> Products()
         {
-            return JsonConvert.DeserializeObject<List<Product>>(Product);
-            ;
+            var path = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).FullName
+                , "Codecool.CodecoolShop\\Storage\\Products.json");
+
+            using (StreamReader r = new StreamReader(path))
+            {
+                var jsonString = await r.ReadToEndAsync();
+                var products = JsonConvert.DeserializeObject<List<Product>>(jsonString);
+                return products;
+            }
         }
     }
 }
--- src/Codecool.CodecoolShop/Controllers/ProductController.cs
+++ src/Codecool.CodecoolShop/Controllers/ProductController.cs
@@ -3,32 +3,28 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Codecool.CodecoolShop.Daos;
-using Codecool.CodecoolShop.Daos.Implementations;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Codecool.CodecoolShop.Models;
-using Codecool.CodecoolShop.Services;
 
 namespace Codecool.CodecoolShop.Controllers
 {
     public class ProductController : Controller
     {
         private readonly ILogger<ProductController> _logger;
-        public ProductService ProductService { get; set; }
+        
 
         public ProductController(ILogger<ProductController> logger)
         {
             _logger = logger;
-            ProductService = new ProductService(
-                ProductDaoMemory.GetInstance(),
-                ProductCategoryDaoMemory.GetInstance());
+            
         }
 
         public IActionResult Index()
         {
-            var products = ProductService.GetProductsForCategory(1);
-            return View(products.ToList());
+            
+            return View();
         }
 
         public IActionResult Privacy()
