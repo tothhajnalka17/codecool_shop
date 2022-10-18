30ac46e62278b67261127b1905210fdccb36b0f4
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-10-18 16:37:25 +0200

    Merge: defeating the mergedemon!

--- src/Codecool.CodecoolShop/Controllers/ProductController.cs
+++ src/Codecool.CodecoolShop/Controllers/ProductController.cs
@@ -3,28 +3,32 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Codecool.CodecoolShop.Daos;
+using Codecool.CodecoolShop.Daos.Implementations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Codecool.CodecoolShop.Models;
+using Codecool.CodecoolShop.Services;
 
 namespace Codecool.CodecoolShop.Controllers
 {
     public class ProductController : Controller
     {
         private readonly ILogger<ProductController> _logger;
-        
+        public ProductService ProductService { get; set; }
 
         public ProductController(ILogger<ProductController> logger)
         {
             _logger = logger;
-            
+            ProductService = new ProductService(
+                ProductDaoMemory.GetInstance(),
+                ProductCategoryDaoMemory.GetInstance());
         }
 
         public IActionResult Index()
         {
-            
-            return View();
+            var products = ProductService.GetProductsForCategory(1);
+            return View(products.ToList());
         }
 
         public IActionResult Privacy()
