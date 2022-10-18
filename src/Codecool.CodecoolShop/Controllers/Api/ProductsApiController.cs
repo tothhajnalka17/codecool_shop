a603ab01202ea612a57b85262bbc197065fb4c50
Dfulop98 <dobo.fulop@icloud.com>
2022-10-18 16:05:56 +0200

    Feat : api controller for all products (Dao system)

--- src/Codecool.CodecoolShop/Controllers/Api/ProductsApiController.cs
+++ src/Codecool.CodecoolShop/Controllers/Api/ProductsApiController.cs
@@ -4,22 +4,28 @@ using System.Net.Http.Json;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Codecool.CodecoolShop.Daos.Implementations;
 using Codecool.CodecoolShop.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
+
+
 namespace Codecool.CodecoolShop.Controllers.Api
 {
+            
     [Route("Products")]
     [ApiController]
     public class ProductApiController : Controller
     {
-
-        public async Task<List<Product>> Products()
+        
+        public IEnumerable<Product> GetProducts()
         {
-            //var path = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).FullName
-            //    , "Codecool.CodecoolShop\\Storage\\Products.json");
-
+            var productDaoMemory = ProductDaoMemory.GetInstance();
+            var products = productDaoMemory.GetAll();
+            
+            return products;
         }
+
     }
 }
