00cbb1752efc1d7327ba54345492a0e5652622c8
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-10-18 10:51:21 +0200

    Add: fetch method for getting the products dynamically

--- src/Codecool.CodecoolShop/wwwroot/js/Model/Model.js
+++ src/Codecool.CodecoolShop/wwwroot/js/Model/Model.js
@@ -1,1 +1,17 @@
-﻿
+﻿export { fetchProducts }
+
+async function fetchProducts() {
+    try {
+        let url = "https://localhost:44334/Products/";
+        //TODO parameterized query
+        //${url}FetchData/?topic=${topic}&page=${page}
+        let response = await fetch(`${url}`);
+        if (response.ok !== true) {
+            throw new Error(`Error making fetch request: ${response}`);
+        } else {
+            return response.json();
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
