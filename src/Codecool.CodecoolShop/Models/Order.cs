82e747cd9ef518b88040bbe00c122a62d4ccbfc0
tothhajnalka17 <tothhajnalka17@gmail.com>
2022-10-18 09:46:59 +0200

    Feat: add Order class when finalizing shopping cart

---
+++ src/Codecool.CodecoolShop/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Codecool.CodecoolShop.Models
+{
+    public class Order
+    {
+        private List<Product> _orderList { get; set; }
+
+        public Order(List<Product> ShoppingCart)
+        {
+            _orderList = ShoppingCart;
+        }
+    }
+}
