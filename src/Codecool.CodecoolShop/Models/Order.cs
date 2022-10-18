4096a6e99cc9b18a9866c57926d2bd63be0d2fca
tothhajnalka17 <tothhajnalka17@gmail.com>
2022-10-18 10:16:54 +0200

    Feat: add PaymentMethod, ShippingMethod enums and user delivery data to Order.cs

--- src/Codecool.CodecoolShop/Models/Order.cs
+++ src/Codecool.CodecoolShop/Models/Order.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System;
 
 namespace Codecool.CodecoolShop.Models
 {
@@ -6,9 +7,28 @@ namespace Codecool.CodecoolShop.Models
     {
         private List<Product> _orderList { get; set; }
 
-        public Order(List<Product> ShoppingCart)
+        private DateTime _orderTime { get; set; }
+        
+        private string _firstName { get; set; }
+
+        private string _lastName { get; set; }
+
+        private ShippingMethod _shippingMethod { get; set; }
+
+        private PaymentMethod _paymenthMethod { get; set; }
+
+        private string _address { get; set; }
+
+        public Order(List<Product> ShoppingCart, string firstName, string lastName, ShippingMethod shippingMethod,
+            PaymentMethod paymentMethod, string address)
         {
             _orderList = ShoppingCart;
+            _orderTime = DateTime.Now;
+            _firstName = firstName;
+            _lastName = lastName;
+            _shippingMethod = shippingMethod;
+            _paymenthMethod = paymentMethod;
+            _address = address;
         }
     }
 }
