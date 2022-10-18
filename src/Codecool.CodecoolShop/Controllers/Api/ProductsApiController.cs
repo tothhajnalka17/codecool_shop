9f1124ecc7f0b9b83e0fbf4ef683c363180e3728
Dfulop98 <dobo.fulop@icloud.com>
2022-10-18 15:34:23 +0200

    Add: api controller for get products

---
+++ src/Codecool.CodecoolShop/Controllers/Api/ProductsApiController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Codecool.CodecoolShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Codecool.CodecoolShop.Controllers.Api
+{
+    [Route("Products")]
+    [ApiController]
+    public class ProductApiController : Controller
+    {
+
+        public async Task<List<Product>> Products()
+        {
+            //var path = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).FullName
+            //    , "Codecool.CodecoolShop\\Storage\\Products.json");
+
+        }
+    }
+}
