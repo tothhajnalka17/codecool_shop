cf800f14dbb04896e6f02b345b703fb8c95061a6
tothhajnalka17 <tothhajnalka17@gmail.com>
2022-10-18 09:47:59 +0200

    Feat: add ShoppingCart class that is created when user adds first item to cart

---
+++ src/Codecool.CodecoolShop/Models/Shoppingcart.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codecool.CodecoolShop.Models
+{
+    public class ShoppingCart
+    {
+        public List<Product> ShoppingCartList { get; private set; }
+
+        public double Total { get; set; }
+
+        public readonly DateTime CreationTime;
+
+        public ShoppingCart()
+        {
+            ShoppingCartList = new List<Product>();
+            Total = 0;
+            CreationTime = DateTime.Now;
+        }
+
+        void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        void Remove(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
