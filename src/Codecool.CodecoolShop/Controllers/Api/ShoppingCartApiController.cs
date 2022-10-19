231667c1c0ffcf676464b7c45134871ec98a5949
ReviKZ <kovacs.zoard.1@gmail.com>
2022-10-19 17:59:11 +0200

    Feat: Add ShoppingCartApiController with routes

---
+++ src/Codecool.CodecoolShop/Controllers/Api/ShoppingCartApiController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Codecool.CodecoolShop.Daos.Implementations;
+using Codecool.CodecoolShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Codecool.CodecoolShop.Controllers.Api
+{
+    [Route("ShoppingCartApi")]
+    [ApiController]
+    public class ShoppingCartApiController : Controller
+    {
+        public ShoppingCartDaoMemory UserShoppingCart { get; set; }
+
+        public ShoppingCartApiController()
+        {
+            UserShoppingCart = ShoppingCartDaoMemory.GetInstance();
+        }
+
+        [Route("Get")]
+        public IEnumerable<Product> GetAll()
+        {
+            var cart = UserShoppingCart.GetAll();
+            return cart;
+        }
+
+        [Route("Add")]
+        public IEnumerable<Product> Add(Product product)
+        {
+            UserShoppingCart.Add(product);
+            var cart = UserShoppingCart.GetAll();
+            return cart;
+        }
+    }
+}
