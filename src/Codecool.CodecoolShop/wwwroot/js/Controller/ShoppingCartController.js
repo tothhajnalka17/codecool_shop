ee1d141c15125f0130179cf08722f100ebb78a18
ReviKZ <kovacs.zoard.1@gmail.com>
2022-10-20 14:21:22 +0200

    Feat: Add RefreshCart functionality [NOW WORKING] Feat: Add AddToCartButtonEventListener functionality [BUGGY]

--- src/Codecool.CodecoolShop/wwwroot/js/Controller/ShoppingCartController.js
+++ src/Codecool.CodecoolShop/wwwroot/js/Controller/ShoppingCartController.js
@@ -1,13 +1,11 @@
-﻿import { fetchProducts } from "../Model/Model.js"
-import { fetchAddProductToCart, fetchCart } from "../Model/ShoppingCartModel.js"
+﻿import { fetchAddProductToCart, fetchCart } from "../Model/ShoppingCartModel.js"
 import { TableFactory, TableRowFactory } from "../View/ShoppingCartView.js"
 
 async function AddToCartButtonEventListener() {
     const addToCartButtons = await document.querySelectorAll('.addToCart')
-    let allproducts = await fetchProducts();
-    await addToCartButtons.forEach((button) => button.addEventListener("click", () => {
-        fetchAddProductToCart(allproducts.Where(product => product.Id == button.parentElement.querySelector('.card-title').innerText.slice(-2, -1)));
-        console.log(fetchCart());
+    await addToCartButtons.forEach((button) => button.addEventListener("click", async () => {
+        await fetchAddProductToCart(1);
+        RefreshCart();
     }))
 }
 
@@ -15,8 +13,9 @@ async function RefreshCart() {
     const container = await document.querySelector('#cart_container');
 
     var cart = await fetchCart();
+    console.log(cart);
 
-    if (cart == null) {
+    if (Object.keys(cart).length == 0) {
         const message = await document.createElement('p');
         message.innerText = "There is nothing in your cart.";
         await container.appendChild(message);
@@ -24,14 +23,23 @@ async function RefreshCart() {
     else {
         const table = await TableFactory();
 
+        await container.appendChild(table);
+
         const tablebody = await document.querySelector("#shopping_cart--body");
 
-        const cartlist = await TableRowFactory();
+        for (var item in cart) {
+            let currentItem = JSON.parse(item);
+            let itemname = await currentItem.Name;
+            let itemcurrency = await currentItem.Currency;
+            let itemprice = await currentItem.DefaultPrice;
+            let quantity = await cart[item];
+            
+            var tablerow = await TableRowFactory(itemname, itemprice, itemcurrency, quantity);
+            tablebody.appendChild(tablerow);
+        }
 
-        await tablebody.parentElement(cartlist);
-
-        await container.appendChild(table);
     }
-
-    AddToCartButtonEventListener();
 }
+
+AddToCartButtonEventListener();
+RefreshCart();
