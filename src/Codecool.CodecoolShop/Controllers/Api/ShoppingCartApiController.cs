a46bcdad54ac2d0b911763b60fddb2b8e420bc11
ReviKZ <kovacs.zoard.1@gmail.com>
2022-10-20 14:17:56 +0200

    Feat: Add serializing to our cart dictionary on the get branch to add quantity for the view model

--- src/Codecool.CodecoolShop/Controllers/Api/ShoppingCartApiController.cs
+++ src/Codecool.CodecoolShop/Controllers/Api/ShoppingCartApiController.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Codecool.CodecoolShop.Daos.Implementations;
 using Codecool.CodecoolShop.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using NuGet.Protocol;
 
 namespace Codecool.CodecoolShop.Controllers.Api
 {
@@ -11,25 +14,42 @@ namespace Codecool.CodecoolShop.Controllers.Api
     public class ShoppingCartApiController : Controller
     {
         public ShoppingCartDaoMemory UserShoppingCart { get; set; }
+        public ProductDaoMemory AllProduct { get; set; }
 
         public ShoppingCartApiController()
         {
             UserShoppingCart = ShoppingCartDaoMemory.GetInstance();
+            AllProduct = ProductDaoMemory.GetInstance();
         }
 
         [Route("Get")]
-        public IEnumerable<Product> GetAll()
+        public string GetAll()
         {
-            var cart = UserShoppingCart.GetAll();
-            return cart;
+            var cart = UserShoppingCart.GetAll().ToList();
+            Dictionary<string, int> cartDictionary = new Dictionary<string, int>();
+
+            for (int i = 0; i < cart.Count(); i++)
+            {
+                if (cartDictionary.ContainsKey(cart[i].ToJson()))
+                {
+                    cartDictionary[cart[i].ToJson()] += 1;
+                }
+                else
+                {
+                    cartDictionary.Add(cart[i].ToJson(), 1);
+                }
+            }
+
+            return cartDictionary.ToJson();
         }
 
         [Route("Add")]
-        public IEnumerable<Product> Add(Product product)
+        public void Add(Dictionary<string, int> dict)
         {
+            var productId = dict["id"];
+
+            var product = AllProduct.GetAll().Where(myproduct => myproduct.Id == productId).First();
             UserShoppingCart.Add(product);
-            var cart = UserShoppingCart.GetAll();
-            return cart;
         }
     }
 }
