f4c744bdf8139e1984166f590a0485c483a6d931
Dfulop98 <dobo.fulop@icloud.com>
2022-11-01 19:48:27 +0100

    Refactor : complette new cardFactory for a new style

--- src/Codecool.CodecoolShop/wwwroot/js/View/View.js
+++ src/Codecool.CodecoolShop/wwwroot/js/View/View.js
@@ -1,59 +1,122 @@
 ﻿export { ButtonFactory, CardFactory }
 import { AddListener } from "../Controller/ShoppingCartController.js"
 
+
 // Create the dom elements that hold the card data
-function CardFactory(name, description, price, currency, id) {
+function CardFactory(name, description, price, id) {
     let card = document.createElement('div');
-    card.classList.add('col-lg-3', 'productCards');
+    card.classList.add("el-wrapper");
+
+    // card header
+    let cardHeaderBox = document.createElement('div');
+    cardHeaderBox.classList.add('box-up');
+    cardHeaderBox.setAttribute("data-id", id);
 
     let image = document.createElement('img');
     image.src = `img/${ name }.jpg`;
-    image.classList.add('productImage');
+    image.classList.add('img');
 
-    let cardBody = document.createElement('div');
-    cardBody.classList.add('card-body');
+    let imageInfo = document.createElement('div');
+    imageInfo.classList.add('img-info');
+
+    let infoInner = document.createElement('div');
+    infoInner.classList.add('info-inner');
+
+    let productName = document.createElement('span');
+    productName.classList.add('p-name');
+    productName.innerText = name;
     
+
+    let productSupplier = document.createElement('span');
+    productSupplier.classList.add('p-company');
+    
+    let productDescription = document.createElement('div');
+    productDescription.classList = 'a-size';
+    productDescription.innerText = "Description";
+
+    let descriptionDetails = document.createElement('span');
+    descriptionDetails.classList.add('size')
+    descriptionDetails.innerText = description
+
+
+    // card footer
+    let cardFooterBox = document.createElement('div');
+    cardFooterBox.classList.add('box-down');
+
+    let priceBackground = document.createElement('div');
+    priceBackground.classList.add('h-bg');
+
+    let priceBgInner = document.createElement('div');
+    priceBgInner.classList.add('h-bg-inner');
+
+    let cart = document.createElement('p');
+    cart.classList.add('cart');
+    cart.setAttribute("data-id", id);
+
+    let priceTag = document.createElement('span');
+    priceTag.classList.add('price');
+    priceTag.innerText = price;
+
+    let addToCart = document.createElement('span');
+    addToCart.classList.add('add-to-cart');
+    AddListener(addToCart);
     
 
-    let cardTitle = document.createElement('h5');
-    cardTitle.classList.add('card-title', 'text-center', 'headerAlign');
-    cardTitle.innerText = name;
-    cardTitle.setAttribute("data-id", id);
-
-    let cardtext1 = document.createElement('p');
-    cardtext1.classList.add('card-text');
-    cardtext1.innerText = description;
-    cardtext1.classList.add('hidedText');
+    let addToCartTxt = document.createElement('span');
+    addToCartTxt.classList.add('txt');
+    addToCartTxt.innerText = "Add in cart"
 
 
-    let cardtext4 = document.createElement('strong');
-    cardtext4.classList.add('card-text', 'priceAlign');
-    cardtext4.innerText = `Price: ${ price } ${ currency }`;
+    // build structure
+    infoInner.appendChild(productName);
+    infoInner.appendChild(productSupplier);
 
-    let button = document.createElement('a');
-    button.classList.add('btn', 'btn-primary', 'addToCart');
-    button.setAttribute('data-Id', id)
-    button.innerText = 'Add To Cart';
-    AddListener(button);
+    productDescription.appendChild(descriptionDetails);
 
-    cardBody.appendChild(cardTitle);
-    cardBody.appendChild(cardtext1);
+    imageInfo.appendChild(infoInner);
+    imageInfo.appendChild(productDescription);
 
-    cardBody.appendChild(cardtext4);
+    cardHeaderBox.appendChild(image);
+    cardHeaderBox.appendChild(imageInfo);
 
-    card.appendChild(image);
-    card.appendChild(cardBody);
-    card.appendChild(button);
+    priceBackground.appendChild(priceBgInner);
 
+    addToCart.appendChild(addToCartTxt);
+
+    cart.appendChild(priceTag);
+    cart.appendChild(addToCart);
+
+    cardFooterBox.appendChild(priceBackground);
+    cardFooterBox.appendChild(cart);
+
+    card.appendChild(cardHeaderBox);
+    card.appendChild(cardFooterBox);
+    
     return card;
 }
 
 function ButtonFactory(filterType, filterValue) {
-    let button = document.createElement('button');
-    button.classList.add('filterButton', 'btn', 'btn-primary');
-    button.setAttribute('toggled', false);
-    button.dataset.filterType = filterType;
-    button.dataset.filterValue = filterValue;
-    button.innerText = filterValue;
-    return button;
+   
+    let filterLabel = document.createElement('label');
+    filterLabel.classList.add('toggle');
+   
+    let filterCheckBox = document.createElement('input');
+    filterCheckBox.classList.add('toggle-checkbox');
+    filterCheckBox.type = "checkbox";
+    filterCheckBox.checked = false;
+
+    let filterSwitch = document.createElement('div');
+    filterSwitch.classList.add('toggle-switch');
+    filterSwitch.setAttribute('filter-type', filterType);
+
+    let filterToggleLable = document.createElement('span');
+    filterToggleLable.classList.add('toggle-label');
+    filterToggleLable.innerText = filterValue;
+
+    filterLabel.appendChild(filterCheckBox);
+    filterLabel.appendChild(filterSwitch);
+    filterLabel.appendChild(filterToggleLable);
+   
+    return filterLabel;
 }
+
