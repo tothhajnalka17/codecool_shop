f0d34aa8f32e263aace853e7ab47b9fbb3732718
Gergely Kamarás <gergely.kamaras@gmail.com>
2022-11-02 17:21:54 +0100

    Add: working user registration

--- src/Codecool.CodecoolShop/Sql/init_db.sql
+++ src/Codecool.CodecoolShop/Sql/init_db.sql
@@ -17,7 +17,7 @@ CREATE TABLE users (
     id int IDENTITY(1,1) PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
 );
 
 CREATE TABLE categories (
