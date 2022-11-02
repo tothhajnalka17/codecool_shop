0ee3edf61c384cba6787b1702e75da8726c9b792
Gudmon <gudmonmarcell@gmail.com>
2022-11-02 16:42:56 +0100

    Feat: validation added for name/email input

--- src/Codecool.CodecoolShop/Controllers/RegistrationController.cs
+++ src/Codecool.CodecoolShop/Controllers/RegistrationController.cs
@@ -23,17 +23,14 @@ namespace Codecool.CodecoolShop.Controllers
         [ActionName("Register")]
         public IActionResult Register(Registration registration)
         {
-            
-            // Check if the username/email is already taken
-            string ID = HttpContext.Session.GetString("ID");
-            Console.WriteLine(ID);
-            Console.WriteLine(registration.Name);
-            Console.WriteLine(registration.Email);
-            var hashedPassword = Crypto.HashPassword(registration.Password);
 
-            //var verified = Crypto.VerifyHashedPassword(hash, "foo");
-            Console.WriteLine(hashedPassword);
-            
+            // Check if the username/email is already taken
+
+            // If not, make new User with the session id, hashed password
+
+            _registrationService.AddUser(registration);
+
+
 
             return Redirect("/Product/Index");
         }
