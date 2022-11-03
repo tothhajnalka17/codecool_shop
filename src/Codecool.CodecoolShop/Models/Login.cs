ff295d30aabb3183caf8ed4141f2768e38b8610e
Gudmon <gudmonmarcell@gmail.com>
2022-11-03 11:36:56 +0100

    Delete: deleting Data folder with CodeCoolShopContext DB

---
+++ src/Codecool.CodecoolShop/Models/Login.cs
@@ -0,0 +1,21 @@
+﻿namespace Codecool.CodecoolShop.Models
+{
+    public class Login
+    {
+        //[Required(ErrorMessage = "Email is required"), MinLength(1), DataType(DataType.EmailAddress), MaxLength(50), Display(Name = "Email:")]
+        //[RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Please enter a valid e-mail adress")]
+        public string Email { get; set; }
+
+        //[DisplayName("Password: ")]
+        //[StringLength(50, MinimumLength = 2)]
+        //[Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        public Login(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+    }
+
+}
