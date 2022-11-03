225b59097203b5053a4b7960df052c61dc3911e7
ReviKZ <kovacs.zoard.1@gmail.com>
2022-11-03 14:06:24 +0100

    Feat: Add JS file Feat: Add queryselectors Feat: Add page modifying function Feat: Add address copying function

---
+++ src/Codecool.CodecoolShop/wwwroot/js/Controller/CheckOutController.js
@@ -0,0 +1,35 @@
+﻿function ModifyAddress() {
+    const shippingdiv = document.querySelector('#shipping');
+
+    const submitbutton = document.querySelector("#checkOutSubmit");
+
+    const checkbox = document.querySelector('#isShippingBilling');
+
+    const shippingdata = document.querySelectorAll('.shipping');
+
+    const billingdata = document.querySelectorAll('.billing');
+
+
+
+    checkbox.addEventListener('change', function () {
+        if (this.checked) {
+            shippingdiv.setAttribute("hidden", "hidden");
+        } else {
+            shippingdiv.removeAttribute("hidden");
+        }
+    })
+
+    submitbutton.addEventListener('click', function () {
+        if (checkbox.checked) {
+            shippingdata[0].value = billingdata[0].value;
+            shippingdata[1].value = billingdata[1].value;
+            shippingdata[2].value = billingdata[2].value;
+            shippingdata[3].value = billingdata[3].value;
+        }
+    })
+
+    
+
+}
+
+ModifyAddress();
