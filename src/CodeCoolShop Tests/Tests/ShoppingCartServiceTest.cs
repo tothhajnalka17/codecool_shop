0085297caa048ba5ff9a5cead7d9ef1910557997
tothhajnalka17 <tothhajnalka17@gmail.com>
2022-11-03 14:25:10 +0100

    Feat: add work on unfinished tests

--- src/CodeCoolShop Tests/Tests/ShoppingCartServiceTest.cs
+++ src/CodeCoolShop Tests/Tests/ShoppingCartServiceTest.cs
@@ -1,18 +1,30 @@
 using Codecool.CodecoolShop.Daos;
+using Codecool.CodecoolShop.Models;
+using Codecool.CodecoolShop.Services;
+using NSubstitute;
 
 namespace CodeCoolShop_Tests
 {
     public class ShoppingCartServiceTests
-    {
+    { 
         [SetUp]
         public void Setup()
         {
+            //_cart = Substitute.For<ShoppingCartService>(Substitute.For<ProductCategory>());
         }
 
         [Test]
         public void GetCartByID_GetsString_ThrowsError()
         {
-            Assert.Pass();
+            // Arrange
+
+
+            // Act
+            //var product = _cart.GetCartById("1").Returns(NotImplementedException);
+
+            // Assert
+
+            //Assert.Throws<NotImplementedException>(_cart.GetCartById("1"));
         }
 
         [Test]
