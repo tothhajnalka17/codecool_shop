ff75581b099def4d1494333e358b99825e22ccd2
ReviKZ <kovacs.zoard.1@gmail.com>
2022-11-03 10:41:49 +0100

    Feat: Add configuration environment

--- src/Codecool.CodecoolShop/Program.cs
+++ src/Codecool.CodecoolShop/Program.cs
@@ -13,6 +13,10 @@ namespace Codecool.CodecoolShop
     {
         public static void Main(string[] args)
         {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
             CreateHostBuilder(args).Build().Run();
         }
 
