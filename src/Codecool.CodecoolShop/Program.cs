b99c8c73f2d5faaff3386f5ce9b415310ad19483
ReviKZ <kovacs.zoard.1@gmail.com>
2022-11-03 10:48:14 +0100

    Feat: Add logger created from environment

--- src/Codecool.CodecoolShop/Program.cs
+++ src/Codecool.CodecoolShop/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace Codecool.CodecoolShop
 {
@@ -16,6 +17,10 @@ namespace Codecool.CodecoolShop
             var configuration = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json")
                 .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
 
             CreateHostBuilder(args).Build().Run();
         }
